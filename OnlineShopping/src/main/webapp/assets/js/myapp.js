274f7e38537e53362565f5024abda0df494b7e22
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-01-20 15:06:05 +0530

    Product table tested

--- OnlineShopping/src/main/webapp/assets/js/myapp.js
+++ OnlineShopping/src/main/webapp/assets/js/myapp.js
@@ -22,5 +22,38 @@ $(function() {
 		break;
 
 	}
-	// window.alert(menu);
+
+	// code for jquery datatables
+	// create a dataset
+
+	var products = [ 	
+						[ '1', 'ABC1' ],
+						[ '2', 'ABC2' ], 
+						[ '3', 'ABC3' ],
+						[ '4', 'ABC4' ], 
+						[ '5', 'ABC5' ], 
+						[ '6', 'ABC6' ], 
+						[ '7', 'ABC7' ],
+						[ '8', 'ABC8' ]
+					];
+	
+	var $table = $('#productListTable');
+	
+	//execute the below code only where we have this table
+	
+	if($table.length){
+		
+		//console.log('inside the table!');
+		
+		$table.DataTable({
+			
+			lengthMenu: [[3,5,10,-1],['3 Records','5 Records','10 Records','All Records']],
+			pageLength:5,
+			data: products
+		});
+	}
+	
+	
+	
+
 });
