7d30c21b34c94ab2fe659e9b864421dc86681098
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-03-04 18:42:06 +0530

    admin datatable

--- OnlineShopping/src/main/java/com/deepanshu/OnlineShopping/validator/ProductValidator.java
+++ OnlineShopping/src/main/java/com/deepanshu/OnlineShopping/validator/ProductValidator.java
@@ -21,7 +21,7 @@ public class ProductValidator implements Validator {
 			errors.rejectValue("file", null, "Please select an image file to upload!");
 			return;
 		}
-		if (!(product.getFile().getContentType().equals("image/jpeg")
+		if (!(product.getFile().getContentType().equals("image /jpeg")
 				|| product.getFile().getContentType().equals("image/png")
 				|| product.getFile().getContentType().equals("image/gif"))) {
 
