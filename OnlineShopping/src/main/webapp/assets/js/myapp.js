786c72acee537c64e437c4fc7ae6222a624bfee6
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-03-28 23:14:57 +0530

    category form jquery validation

--- OnlineShopping/src/main/webapp/assets/js/myapp.js
+++ OnlineShopping/src/main/webapp/assets/js/myapp.js
@@ -225,7 +225,9 @@ $(function() {
 								mRender : function(data, type, row) {
 									var str = '';
 
-									str += '<a href="'+window.contextRoot+'/manage/'
+									str += '<a href="'
+											+ window.contextRoot
+											+ '/manage/'
 											+ data
 											+ '/product" class="btn btn-outline-primary">';
 									str += '<i class="fa fa-fw fa-edit"></i></a>';
@@ -292,6 +294,44 @@ $(function() {
 										});
 					}
 				});
+	}
+
+	// ---------------------
+	// validation code for category
+
+	var $categoryForm = $('#categoryForm');
+
+	if ($categoryForm.length) {
+		$categoryForm
+				.validate({
+					rules : {
+						name : {
+							required : true,
+							minlength : 2
+						},
+						description : {
+							required : true
+						}
+					},
+					messages : {
+						name : {
+							required : 'Please add the category name!',
+							minlength : 'The category name should not be less than 2 characters'
+						},
+
+						description : {
+							required : 'PLease add a description for this category!'
+						}
+					},
+					errorElement : 'em',
+					errorPlacement : function(error, element) {
+						// add the class of help-block
+						error.addClass('help-block');
+						// add the error element after the input element
+						error.insterAfter(element);
+					}
+				});
+
 	}
 
 });
