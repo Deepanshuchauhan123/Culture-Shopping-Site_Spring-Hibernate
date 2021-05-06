1ab7922e334f05d6c2b3bcc1010ebf646a204ba5
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-05-06 18:43:33 +0530

    Adding address mant to one mapping

--- ShoppingBackend/src/main/java/com/deepanshu/ShoppingBackend/dao/UserDAO.java
+++ ShoppingBackend/src/main/java/com/deepanshu/ShoppingBackend/dao/UserDAO.java
@@ -1,20 +1,26 @@
 package com.deepanshu.ShoppingBackend.dao;
 
+import java.util.List;
+
 import com.deepanshu.ShoppingBackend.dto.Address;
 import com.deepanshu.ShoppingBackend.dto.Cart;
 import com.deepanshu.ShoppingBackend.dto.User;
 
 public interface UserDAO {
 
-	//add a user
+	// add a user
 	boolean addUser(User user);
-	
-	//getting by email
+
+	// getting by email
 	User getByEmail(String email);
-	
-	//add an address
+
+	// add an address
 	boolean addAdress(Address address);
-	
-	//add a cart
+
+	Address getBillingAddress(User user);
+
+	List<Address> listShippingAddresses(User user);
+
+	// add a cart
 	boolean updateCart(Cart cart);
 }
