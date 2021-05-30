8d193bbe43d6c7704f05090e5546724983e2c228
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-05-30 17:33:56 +0530

    signup flow page modeified

--- ShoppingBackend/src/main/java/com/deepanshu/ShoppingBackend/dto/User.java
+++ ShoppingBackend/src/main/java/com/deepanshu/ShoppingBackend/dto/User.java
@@ -1,5 +1,7 @@
 package com.deepanshu.ShoppingBackend.dto;
 
+import java.io.Serializable;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,8 +13,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "user_detail")
-public class User {
+public class User implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
@@ -29,6 +35,16 @@ public class User {
 
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private Cart cart;
+
+	private String confirmPassword;
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
 
 	public Cart getCart() {
 		return cart;
